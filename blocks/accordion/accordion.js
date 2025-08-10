2c10af44eaf41af4038e8cc4bcda9db787f1fc59
mpesintrex <mpesin@trex.com>
2025-08-10 16:08:27 -0400

    card block add classed 5

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,17 +1,17 @@
 export default function decorate(block) {
   block.querySelectorAll(':scope > div').forEach((item) => {
-    const header = item.querySelector('h1, h2, h3, h4, p');
+    // const header = item.querySelector('h1, h2, h3, h4, p');
     const body = item.querySelector('div[data-aue-prop="body"]');
 
-    if (header && body) {
-      header.classList.add('accordion-header');
+    if (body) {
+      // header.classList.add('accordion-header');
       body.classList.add('accordion-body');
       body.style.display = 'none';
 
-      header.addEventListener('click', () => {
+      this.addEventListener('click', () => {
         const expanded = body.style.display === 'block';
         body.style.display = expanded ? 'none' : 'block';
-        header.classList.toggle('open', !expanded);
+        this.classList.toggle('open', !expanded);
       });
     }
   });
