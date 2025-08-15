2fc764c9b5e66875132be124c467e84ce8f5e9d5
mpesintrex <mpesin@trex.com>
2025-08-15 14:44:58 -0400

    update section classes 2

--- scripts/aem.js
+++ scripts/aem.js
@@ -499,7 +499,7 @@ function decorateSections(main) {
     if (sectionMeta) {
       const meta = readBlockConfig(sectionMeta);
       Object.keys(meta).forEach((key) => {
-        if (key === 'style' || key == 'classes') {
+        if (key === 'style') {
           const styles = meta.style
             .split(',')
             .filter((style) => style)
